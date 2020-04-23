effda4ff5ad14660451bfa292f7629f89009a63a
lev <levavakian@gmail.com>
2020-04-23 02:05:52 -0700

    report only csp

--- server/server.py
+++ server/server.py
@@ -4,13 +4,7 @@ from flask_talisman import Talisman
 app = Flask(__name__, static_url_path='',
             static_folder='../client/build',
             template_folder='../client/build')
-csp = {
-    'default-src': [
-        '\'self\'',
-        '*'
-    ]
-}
-Talisman(app, content_security_policy=csp)
+Talisman(app, content_security_policy_report_only=True)
 
 @app.route("/")
 def entrypoint():
