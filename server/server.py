c273e7c1bdf3fa1e72e62d90280728495a12f505
lev <levavakian@gmail.com>
2020-04-23 02:47:52 -0700

    nuke csp

--- server/server.py
+++ server/server.py
@@ -4,38 +4,7 @@ from flask_talisman import Talisman
 app = Flask(__name__, static_url_path='',
             static_folder='../client/build',
             template_folder='../client/build')
-csp = {
-    'default-src': [
-        'unsafe-inline',
-        'self',
-    ],
-    'media-src': [
-        'unsafe-inline',
-        '*',
-    ],
-    'img-src': [
-        'unsafe-inline',
-        '*',
-    ],
-    'font-src': [
-        'unsafe-inline',
-        '*',
-    ],
-    'style-src': [
-        'unsafe-inline',
-        '*',
-    ],
-    'script-src': [
-        'unsafe-inline',
-        '*'
-    ],
-    'manifest-src': [
-        'unsafe-inline',
-        '*'
-    ],
-
-}
-Talisman(app, content_security_policy=csp)
+Talisman(app, content_security_policy=None)
 
 @app.route("/")
 def entrypoint():
